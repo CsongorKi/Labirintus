91e6e6e14a26ba642ceeb2a199d6a4d47f470fc4
Barna Verdes <b.dsadsa.v@gmail.com>
2022-05-16 12:17:25 +0200

    first_commit

---
+++ Labyrinth/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Labyrinth
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game currentGame = new Game();
+            currentGame.Start();
+        }
+    }
+}
